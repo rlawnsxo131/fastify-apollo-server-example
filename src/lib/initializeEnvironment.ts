fe0025f0fc9b57285e8724237d034cd9523571a2
juntae <rlawnsxo131@gmail.com>
2021-09-23 23:50:43 +0900

    small modifications to .gitignore and initializeEnvironment files

--- src/lib/initializeEnvironment.ts
+++ src/lib/initializeEnvironment.ts
@@ -1,10 +1,11 @@
 import { config } from 'dotenv';
 import { resolve } from 'path';
+import { isProduction } from '../constants';
 
-const { NODE_ENV } = process.env;
+const environment = isProduction ? '.env.production' : '.env.development';
 
 export default function initializeEnvironment() {
   config({
-    path: resolve(process.cwd(), `.env.${NODE_ENV}`),
+    path: resolve(process.cwd(), environment),
   });
 }
