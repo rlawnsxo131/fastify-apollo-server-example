9e1eef0a76bcba1c05cc079da2e6180830264396
juntae <rlawnsxo131@gmail.com>
2021-09-25 02:44:16 +0900

    add index to route folder

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,1 @@
-import { FastifyPluginCallback } from 'fastify';
-
-const routes: FastifyPluginCallback = (fastify, opts, done) => {
-  fastify.get('/health', async (request, reply) => {
-    reply.status(200).send({ hello: 'world' });
-  });
-  done();
-};
-
-export default routes;
+export { default } from './routes';
